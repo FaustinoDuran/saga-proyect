ad6e15cf4cd7546adbd8ed05258e52646cef6c35
Ignacio Patino <ignaciocorreagabbi@gmail.com>
2025-12-02 18:36:15 -0300

    compras routes terminado

--- ms-compras/src/routes/compras.routes.ts
+++ ms-compras/src/routes/compras.routes.ts
@@ -1,1 +1,12 @@
+import { Router } from "express";
+import { CompraController } from "../controller/compras.controller";
+
+
+const router = Router();
+const comprasController = new CompraController;
+
+router.post('/transaccion', comprasController.procesarTransaccion);
+router.post('/compensar', comprasController.compensarCompra);
+
+export default router;
 
